f10aeb03a7bb528cbcf8aa6d3b71b57c99eab35c
Mariusz Pisz <kontakt@mariuszpisz.pl>
2020-11-14 17:13:20 +0100

    Working maven-jar-plugin

---
+++ src/main/java/pl/hudyweas/testproject/Project.java
@@ -0,0 +1,10 @@
+package pl.hudyweas.testproject;
+
+public class Project {
+
+    public static void main(String[] args) {
+        Program program = new Program();
+        program.start();
+    }
+
+}
