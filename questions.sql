1d8435b5c47037c9f32704f821ef0ff2a8909b78
hudyweas <73641290+hudyweas@users.noreply.github.com>
2020-11-24 21:57:58 +0100

    Add files via upload

--- questions.sql
+++ questions.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 22 Lis 2020, 19:13
+-- Czas generowania: 24 Lis 2020, 21:56
 -- Wersja serwera: 10.4.11-MariaDB
 -- Wersja PHP: 7.2.27
 
@@ -32,8 +32,29 @@ CREATE TABLE `answers` (
   `id` int(11) NOT NULL,
   `contenct` text NOT NULL,
   `isCorrect` int(1) NOT NULL,
-  `id_question` int(11) NOT NULL
+  `question_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `answers`
+--
+
+INSERT INTO `answers` (`id`, `contenct`, `isCorrect`, `question_id`) VALUES
+(1, 'tak', 0, 1),
+(2, 'nie', 1, 1),
+(3, 'jest bardzo plaska', 0, 1),
+(4, 'zdecydowanie nie jest', 1, 1),
+(5, 'tak', 1, 2),
+(6, 'nie', 0, 2),
+(7, '2', 1, 5),
+(8, '-2', 1, 5),
+(9, '1/2', 0, 5),
+(11, 'tak', 1, 3),
+(12, 'nie', 0, 3),
+(13, 'wynosi 3', 0, 3),
+(14, 'wynosi 1/2', 0, 3),
+(15, 'tak', 1, 4),
+(16, 'nie', 0, 4);
 
 -- --------------------------------------------------------
 
@@ -47,6 +68,17 @@ CREATE TABLE `questions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Zrzut danych tabeli `questions`
+--
+
+INSERT INTO `questions` (`id`, `content`) VALUES
+(1, 'Czy ziemia jest płaska?'),
+(2, 'Czy rok przystepny jest co 4 lata?'),
+(3, 'Czy 1+1 = 2'),
+(4, 'Czy po a w alfabecie jest b?'),
+(5, 'Jeśli a^2 = 4 ile wynosi a');
+
+--
 -- Indeksy dla zrzutów tabel
 --
 
@@ -55,7 +87,7 @@ CREATE TABLE `questions` (
 --
 ALTER TABLE `answers`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_question` (`id_question`);
+  ADD KEY `id_question` (`question_id`);
 
 --
 -- Indeksy dla tabeli `questions`
@@ -71,13 +103,13 @@ ALTER TABLE `questions`
 -- AUTO_INCREMENT dla tabeli `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT dla tabeli `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Ograniczenia dla zrzutów tabel
@@ -87,7 +119,7 @@ ALTER TABLE `questions`
 -- Ograniczenia dla tabeli `answers`
 --
 ALTER TABLE `answers`
-  ADD CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`id_question`) REFERENCES `questions` (`id`);
+  ADD CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
